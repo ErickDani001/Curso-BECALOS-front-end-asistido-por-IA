48831674837a9b67cbbe6d53349c708ec1c8ae7a
Erick Dani Avila <ericksin2154@gmail.com>
2025-11-21 22:19:36 -0600

    Mi primer commi-Pagina del Universo

---
+++ Styles/prueba.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class prueba {
+
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+
+        int[] arreglo = new int[3];
+
+        arreglo[0] = 25;
+        arreglo[1] = 18;
+        arreglo[2] = 14;
+
+        System.out.println(arreglo[0] + " " + arreglo[1] + " " + arreglo[2]);
+
+    }
+
+}
