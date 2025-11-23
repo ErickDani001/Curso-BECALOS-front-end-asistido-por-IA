f75d01dcb420175ea4724c9c46b65e5c60bd3054
Erick Dani Avila <ericksin2154@gmail.com>
2025-11-22 21:28:28 -0600

    update

--- Styles/prueba.java
+++ Styles/prueba.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class prueba {
 
     public static void main(String[] args) {
-        Scanner src = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int[] arreglo = new int[3];
 
@@ -11,7 +11,23 @@ public class prueba {
         arreglo[1] = 18;
         arreglo[2] = 14;
 
-        System.out.println(arreglo[0] + " " + arreglo[1] + " " + arreglo[2]);
+        // System.out.println(arreglo[0] + " " + arreglo[1] + " " + arreglo[2]);
+
+        int i = 1;
+
+        while (i < arreglo.length) {
+
+            System.out.println("Ingrese un número: ");
+            int numero = sc.nextInt();
+
+            if (numero <= 0) {
+                System.out.println("El número debe ser mayor a 0");
+            } else {
+                arreglo[i] = numero;
+            }
+
+            i++;
+        }
 
     }
 
